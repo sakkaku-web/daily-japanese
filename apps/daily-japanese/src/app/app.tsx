0e2106034e69dc060d744c059018a284caf401e1
myin <minyin142p@gmail.com>
2022-04-07 18:21:16 +0200

    feat: show news very simple

--- apps/daily-japanese/src/app/app.tsx
+++ apps/daily-japanese/src/app/app.tsx
@@ -1,15 +1,27 @@
 import styles from './app.module.scss';
-import { NewspaperClient } from '@sakkaku-web/api-newspaper-client';
-import { useEffect } from 'react';
+import { News, NewspaperClient } from '@sakkaku-web/api-newspaper-client';
+import { useEffect, useState } from 'react';
 
 const client = new NewspaperClient();
 
 export function App() {
+  const [news, setNews] = useState([] as News[]);
+
   useEffect(() => {
-    client.getNews().then((x) => console.log(x));
+    client.getNews().then(setNews);
   });
 
-  return <div></div>;
+  return (
+    <div>
+      <ul>
+        {news.map((x) => (
+          <li>
+            <a href={x.url}>{x.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
