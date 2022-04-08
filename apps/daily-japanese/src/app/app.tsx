4af02e456d49600723c32b10423a551291880ba2
Hao <74541600+haod98@users.noreply.github.com>
2022-04-08 19:25:03 +0200

    feat: show articles

--- apps/daily-japanese/src/app/app.tsx
+++ apps/daily-japanese/src/app/app.tsx
@@ -10,12 +10,25 @@ export function App() {
     client.getNews().then(setNews);
   }, []);
 
+  console.log(news);
+
   return (
-    <div>
+    <div className="p-3">
+      <h1 className="text-center">DailyJapanese</h1>
       <ul>
         {news.map((x, i) => (
-          <li key={i}>
-            <a href={x.url}>{x.title}</a>
+          <li key={i} className="border-2 border-cyan-500 mb-3">
+            <div className="mb-2">
+              <a href={x.url}>
+                <img src={x.urlToImage} alt={x.title} />
+              </a>
+            </div>
+            <div className="px-4">
+              <h2 className="text-2xl font-bold">
+                <a href={x.url}>{x.title}</a>
+                <p className="font-light text-base mt-4">{x.description}</p>
+              </h2>
+            </div>
           </li>
         ))}
       </ul>
