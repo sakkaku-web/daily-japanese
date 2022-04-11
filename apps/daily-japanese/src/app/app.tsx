3ca33653748f3bb9180eb297935a26c5a11b7361
myin <minyin142p@gmail.com>
2022-04-11 16:39:06 +0200

    refactor: add routing

--- apps/daily-japanese/src/app/app.tsx
+++ apps/daily-japanese/src/app/app.tsx
@@ -1,36 +1,18 @@
-import { News, NewspaperClient } from '@sakkaku-web/api-newspaper-client';
-import { useEffect, useState } from 'react';
+import { NewspaperClient } from '@sakkaku-web/api-newspaper-client';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { NewsPage } from './pages/news-page';
 
 const client = new NewspaperClient();
 
 export function App() {
-  const [news, setNews] = useState([] as News[]);
-
-  useEffect(() => {
-    client.getNews().then(setNews);
-  }, []);
-
   return (
     <div className="p-5 text-white md:mx-[20%] lg:mx-[35%]">
       <h1 className="text-center text-2xl mb-3 font-bold">Daily Japanese</h1>
-      <ul className="flex flex-col flex-wrap gap-4">
-        {news.map((x, i) => (
-          <li key={i} className="rounded-md bg-gray-800">
-            <a href={x.url}>
-              <img
-                src={x.urlToImage}
-                className="max-h-52 rounded-tl-md rounded-tr-md object-cover w-full"
-                alt=""
-              />
-              <div className="px-4 py-2">
-                <p className="mb-2 text-xs text-white/50">{x.publishedAt}</p>
-                <h2 className="text-2xl font-bold">{x.title}</h2>
-                <p className="font-light text-base mt-4">{x.description}</p>
-              </div>
-            </a>
-          </li>
-        ))}
-      </ul>
+
+      <Routes>
+        <Route path="/news" element={<NewsPage client={client} />} />
+        <Route path="/" element={<Navigate to="/news" />} />
+      </Routes>
     </div>
   );
 }
---
+++ apps/daily-japanese/src/app/pages/news-page.tsx
@@ -0,0 +1,35 @@
+import { News, NewspaperClient } from '@sakkaku-web/api-newspaper-client';
+import { useState, useEffect } from 'react';
+
+interface NewsPageProps {
+  client: NewspaperClient;
+}
+
+export function NewsPage({ client }: NewsPageProps) {
+  const [news, setNews] = useState([] as News[]);
+
+  useEffect(() => {
+    client.getNews().then(setNews);
+  }, []);
+
+  return (
+    <ul className="flex flex-col flex-wrap gap-4">
+      {news.map((x, i) => (
+        <li key={i} className="rounded-md bg-gray-800">
+          <a href={x.url}>
+            <img
+              src={x.urlToImage}
+              className="max-h-52 rounded-tl-md rounded-tr-md object-cover w-full"
+              alt=""
+            />
+            <div className="px-4 py-2">
+              <p className="mb-2 text-xs text-white/50">{x.publishedAt}</p>
+              <h2 className="text-2xl font-bold">{x.title}</h2>
+              <p className="font-light text-base mt-4">{x.description}</p>
+            </div>
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+}
