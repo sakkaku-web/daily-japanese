371cb2c44796c1505e308a532b12bad67944d858
myin <minyin142p@gmail.com>
2022-04-11 18:45:58 +0200

    fix: add base href for prod

--- apps/daily-japanese/src/main.tsx
+++ apps/daily-japanese/src/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/app';
+import { environment } from './environments/environment';
 
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={environment.baseHref}>
       <App />
     </BrowserRouter>
   </StrictMode>,
