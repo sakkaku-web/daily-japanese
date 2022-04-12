5f7d6f24d5c4fe5528caac66be8e4381429779b9
myin <minyin142p@gmail.com>
2022-04-12 17:55:44 +0200

    feat(#10): add daily word with category select

---
+++ apps/daily-japanese/src/app/components/daily-word-category-select.tsx
@@ -0,0 +1,25 @@
+interface DailyWordCategorySelectProps {
+  category: string;
+  categories: string[];
+  onCategorySelect: (cat: string) => void;
+}
+
+export function DailyWordCategorySelect({
+  category,
+  categories,
+  onCategorySelect,
+}: DailyWordCategorySelectProps) {
+  return (
+    <select
+      className="text-center mt-4"
+      value={category}
+      onChange={(e) => onCategorySelect(e.target.value)}
+    >
+      {categories.map((cat) => (
+        <option key={cat} value={cat}>
+          {cat}
+        </option>
+      ))}
+    </select>
+  );
+}
--- apps/daily-japanese/src/app/pages/daily-word-page.tsx
+++ apps/daily-japanese/src/app/pages/daily-word-page.tsx
@@ -2,36 +2,59 @@ import { DailyWord, NewspaperClient } from '@sakkaku-web/api-newspaper-client';
 import { JapaneseWordle } from '@sakkaku-web/components/japanese-wordle';
 import { add } from 'date-fns';
 import { useEffect, useState } from 'react';
+import { DailyWordCategorySelect } from '../components/daily-word-category-select';
+import { DailyWordComponent } from '../components/daily-word-component';
 
 interface DailyWordPageProps {
   client: NewspaperClient;
 }
 
 export function DailyWordPage({ client }: DailyWordPageProps) {
+  const [dailyWords, setDailyWords] = useState([] as DailyWord[]);
   const [nextDailyWords, setNextDailyWords] = useState([] as DailyWord[]);
+  const [selectedCategory, setSelectedCategory] = useState('');
 
   useEffect(() => {
     const today = new Date();
+    client.getDailyWords(today).then((words) => {
+      setDailyWords(words);
+      setSelectedCategory(words[0]?.category);
+    });
     client.getDailyWords(add(today, { days: 1 })).then(setNextDailyWords);
   }, []);
-
-  const getWordForWordle = () => {
-    return nextDailyWords[0]?.reading;
-  };
 
   const getAttemptsForWord = (word: string) => {
     return word.length * 2;
   };
 
-  const wordleWord = getWordForWordle();
+  const wordleWord = nextDailyWords.find(
+    (w) => w.category === selectedCategory
+  )?.reading;
+  const word = dailyWords.find((w) => w.category === selectedCategory);
+  const categories = dailyWords.map((w) => w.category);
+
   return (
-    <div>
+    <div className="flex flex-col gap-8 text-center">
+      <div className="flex flex-col text-center items-center">
+        {word && <DailyWordComponent word={word} />}
+
+        {word && (
+          <DailyWordCategorySelect
+            categories={categories}
+            category={selectedCategory}
+            onCategorySelect={setSelectedCategory}
+          />
+        )}
+      </div>
       {wordleWord && (
-        <JapaneseWordle
-          word={wordleWord}
-          maxAttempts={getAttemptsForWord(wordleWord)}
-          darkMode={true}
-        />
+        <div>
+          <h2 className="font-bold text-2xl my-4">Next Word</h2>
+          <JapaneseWordle
+            word={wordleWord}
+            maxAttempts={getAttemptsForWord(wordleWord)}
+            darkMode={true}
+          />
+        </div>
       )}
     </div>
   );
