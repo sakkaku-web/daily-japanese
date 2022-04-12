7d251c3bc4df735adc229f8cd2be4a9692c72b08
myin <minyin142p@gmail.com>
2022-04-12 18:38:10 +0200

    feat: cache daily words

--- apps/daily-japanese/src/app/pages/daily-word-page.tsx
+++ apps/daily-japanese/src/app/pages/daily-word-page.tsx
@@ -1,6 +1,7 @@
 import { DailyWord, NewspaperClient } from '@sakkaku-web/api-newspaper-client';
 import { JapaneseWordle } from '@sakkaku-web/components/japanese-wordle';
 import { add } from 'date-fns';
+import { isSameDay } from 'date-fns/esm';
 import { useEffect, useState } from 'react';
 import { DailyWordCategorySelect } from '../components/daily-word-category-select';
 import { DailyWordComponent } from '../components/daily-word-component';
@@ -10,6 +11,13 @@ interface DailyWordPageProps {
 }
 
 const DAILY_WORD_CATEGORY_KEY = 'dailyJapanese-dailyWord-category';
+const DAILY_WORD_CACHE = 'dailyJapanese-dailyWord-cache';
+
+interface DailyWordCache {
+  dailyWords: DailyWord[];
+  nextDailyWords: DailyWord[];
+  date: string;
+}
 
 export function DailyWordPage({ client }: DailyWordPageProps) {
   const [dailyWords, setDailyWords] = useState([] as DailyWord[]);
@@ -23,15 +31,45 @@ export function DailyWordPage({ client }: DailyWordPageProps) {
     localStorage.setItem(DAILY_WORD_CATEGORY_KEY, cat);
   };
 
+  const updateDailyWordCache = (data: Partial<DailyWordCache>) => {
+    localStorage.setItem(
+      DAILY_WORD_CACHE,
+      JSON.stringify({
+        dailyWords,
+        nextDailyWords,
+        ...data,
+        date: new Date().toISOString(),
+      })
+    );
+  };
+
   useEffect(() => {
     const today = new Date();
+    const cache = JSON.parse(
+      localStorage.getItem(DAILY_WORD_CACHE) || '{}'
+    ) as DailyWordCache;
+
+    if (
+      isSameDay(today, new Date(cache.date)) &&
+      cache.dailyWords.length &&
+      cache.nextDailyWords.length
+    ) {
+      setDailyWords(cache.dailyWords);
+      setNextDailyWords(cache.nextDailyWords);
+      return;
+    }
+
     client.getDailyWords(today).then((words) => {
       setDailyWords(words);
       if (selectedCategory == null) {
         setSelectedCategory(words[0]?.category);
       }
+      updateDailyWordCache({ dailyWords: words });
     });
-    client.getDailyWords(add(today, { days: 1 })).then(setNextDailyWords);
+    client.getDailyWords(add(today, { days: 1 })).then((words) => {
+      setNextDailyWords(words);
+      updateDailyWordCache({ nextDailyWords: words });
+    });
   }, []);
 
   const getAttemptsForWord = (word: string) => {
